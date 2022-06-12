6662b5c6e3951fca4cf9ab0e79a847e61328c9d4
mdbow22 <mdbow22@gmail.com>
2022-06-11 21:21:38 -0400

    fix authentication

--- budget-me-react/src/services/Private.tsx
+++ budget-me-react/src/services/Private.tsx
@@ -1,21 +1,21 @@
-import React, { ReactNode } from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { ReactNode, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useUser } from './UserContext';
+import { getToken } from '../utils/Auth';
 
 const Private: React.FC<{ children: ReactNode}> = ({ children }) => {
 
+    const navigate = useNavigate();
     const { isAuthed } = useUser();
 
-    if(isAuthed) {
-        return (
-            <>
-            {children}
-            </>)
+    if(!isAuthed) {
+        navigate(`/login`); 
     }
 
   return (
-    <Navigate to='/login' />
-  )
+    <>
+    {children}
+    </>)
 }
 
 export default Private;

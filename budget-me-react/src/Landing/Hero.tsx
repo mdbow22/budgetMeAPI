4b8b088f758e7197fed08d5cf49d04371cd532b3
mdbow22 <mdbow22@gmail.com>
2022-06-11 22:26:10 -0400

    redirect to dash if already logged in

--- budget-me-react/src/Landing/Hero.tsx
+++ budget-me-react/src/Landing/Hero.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useUser } from '../services/UserContext';
 import undraw_investment from './undraw_investment.svg';
 
 const Hero: React.FC = () => {
+
+    const navigate = useNavigate();
+    const { isAuthed, user } = useUser();
+    
+    useEffect(() => {
+        if(isAuthed) {
+            navigate(`/user/${user?.id}/dashboard`);
+        }
+    }, [isAuthed, user?.id])
+
   return (
     <div className='flex flex-col max-w-5xl mx-auto px-3 py-10'>
         <div className='w-100 border landing-hero flex md:flex-row flex-col justify-center md:justify-between items-center py-3 bg-gradient-to-bl from-green-50 shadow-lg'>

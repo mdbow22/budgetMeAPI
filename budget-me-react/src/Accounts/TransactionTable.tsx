1010637879aa60927f17e8d2e2e0b88510de60c9
mdbow22 <mdbow22@gmail.com>
2022-06-20 16:26:26 -0400

    hide columns on small screens

--- budget-me-react/src/Accounts/TransactionTable.tsx
+++ budget-me-react/src/Accounts/TransactionTable.tsx
@@ -24,39 +24,41 @@ interface TransTableProps {
 
 const TransactionTable: React.FC<TransTableProps> = ({ transactions }) => {
   return (
-    <table className='w-full mt-4 border border-gray-400'>
-        <thead className='text-left'>
-            <tr className='trans-table__header border-b-2 border-gray-400 bg-gray-200'>
-                <th className='w-1/8 px-2 py-3 '>Date</th>
-                <th className='w-1/4 px-2 py-3'>Description</th>
-                <th className='w-1/6 px-2 py-3'>Payer/Payee</th>
-                <th className='w-1/6 px-2 py-3'>Category</th>
-                <th className='w-1/6 px-2 py-3'>Account</th>
-                <th className='w-1/8 px-2 py-3'>Amount</th>
-            </tr>
-        </thead>
-        <tbody>
-            {transactions?.map(transaction => {
-                return (
-                    <tr key={transaction.id} className='text-sm   even:bg-gray-200'>
-                        <td className='px-2 py-1 '>{DateTime.fromISO(transaction.date).toFormat('MM/dd/yyyy')}</td>
-                        <td className='px-2 py-1 '>{transaction.description}</td>
-                        <td className='px-2 py-1 '>{transaction.third_party}</td>
-                        <td className='px-2 py-1 '>{transaction.category}</td>
-                        <td className='px-2 py-1 '>{transaction.Account.name}</td>
-                        <td className='px-2 py-1 flex justify-between'>
-                            <div>
-                                $
-                            </div>
-                            <div>
-                                {transaction.amount}
-                            </div>
-                        </td>
-                    </tr>
-                )
-            })}
-        </tbody>
-    </table>
+    <div style={{minHeight: '400px'}} className='w-full border border-gray-400 mt-4'>
+        <table className='w-full'>
+            <thead className='text-left'>
+                <tr className='trans-table__header border-b-2 border-gray-400 bg-gray-200'>
+                    <th className='w-1/8 px-2 py-3 '>Date</th>
+                    <th className='w-1/4 px-2 py-3'>Description</th>
+                    <th className='w-1/6 px-2 py-3 hidden md:table-cell'>Payer/Payee</th>
+                    <th className='w-1/6 px-2 py-3 hidden md:table-cell'>Category</th>
+                    <th className='w-1/6 px-2 py-3'>Account</th>
+                    <th className='w-1/8 px-2 py-3'>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                {transactions?.map(transaction => {
+                    return (
+                        <tr key={transaction.id} className='text-sm   even:bg-gray-200'>
+                            <td className='px-2 py-1 '>{DateTime.fromISO(transaction.date).toFormat('MM/dd')}</td>
+                            <td className='px-2 py-1 '>{transaction.description}</td>
+                            <td className='px-2 py-1 hidden md:table-cell'>{transaction.third_party}</td>
+                            <td className='px-2 py-1 hidden md:table-cell'>{transaction.category}</td>
+                            <td className='px-2 py-1 '>{transaction.Account.name}</td>
+                            <td className='px-2 py-1 flex justify-between'>
+                                <div>
+                                    $
+                                </div>
+                                <div>
+                                    {transaction.amount}
+                                </div>
+                            </td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    </div>
   )
 }
 
